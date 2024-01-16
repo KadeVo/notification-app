5469c3a77946a6ef13d8feb30973e225de724c58
Kadin Vo <kadinvo@gmail.com>
2024-01-16 21:17:46 +1300

    Added request for notification permission

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import * as Notifications from 'expo-notifications'
 
 export default function App() {
+  useEffect(() => {
+    ;(async () => {
+      const { status } = await Notifications.requestPermissionsAsync()
+      if (status !== 'granted') {
+        alert('Notification permissions are required!')
+      }
+    })()
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +27,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
